e8158c1b6bbab27c94e60a61f601c28ae7bc094b
irfanca1 <irfanchoiruddin23@gmail.com>
2023-04-14 22:58:07 +0700

    membuat fitur filter data dan cari data

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const accounting = require('accounting')
 const swal = require('sweetalert2')
 const dayjs = require('dayjs')
 const fs = require('fs')
+const { Op } = require('sequelize');
 const app = express()
 const PORT = process.env.PORT || 5000
 
@@ -43,12 +44,56 @@ app.get('/', async (req, res) => {
         title: 'Halaman Utama',
         message: 'Ini adalah halaman Utama'
     }
-    await Cars.findAll({
-        order: [['id', 'ASC']]
-    })
-        .then(cars => {
-            res.render('index', { data, cars })
+    if (req.query.filter) {
+        await Cars.findAll({
+            where: {
+                ukuran: {
+                    [Op.substring]: req.query.filter
+                }
+            },
+            order: [['id', 'ASC']]
         })
+            .then(cars => {
+                res.render('index', { data, cars })
+            })
+    } else {
+        products = await Cars.findAll({
+            order: [['id', 'ASC']]
+        })
+            .then(cars => {
+                res.render('index', { data, cars })
+            })
+    }
+
+
+})
+
+app.get('/search/car', async (req, res) => {
+    // Cari Mobil 
+    const data = {
+        title: 'Halaman Utama',
+        message: 'Ini adalah halaman Utama'
+    }
+    if (req.query.search) {
+        let searchCar = req.query.search
+        await Cars.findAll({
+            where: {
+                nama_mobil: {
+                    [Op.like]: `%${searchCar}%`
+                }
+            }
+        })
+            .then(cars => {
+                res.render('index', { data, cars, searchCar })
+            })
+    } else {
+        await Cars.findAll({
+            order: [['id', 'ASC']]
+        })
+            .then(cars => {
+                res.render('index', { data, cars })
+            })
+    }
 })
 
 app.get('/add', (req, res) => {
